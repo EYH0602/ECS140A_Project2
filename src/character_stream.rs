4e83810f525e14988d311735c602d38b67e14095
Ethan He <yfhe@ucdavis.edu>
2021-10-24 14:13:59 -0700

    implement char stream

--- src/character_stream.rs
+++ src/character_stream.rs
@@ -1,24 +1,39 @@
+use std::convert::TryFrom;
 use std::fs::File;
 use std::io;
-use std::convert::TryFrom;
 use std::io::prelude::*;
 
-
 pub struct CharStream {
+	contents: String,
 }
 
 impl CharStream {
-
 	pub fn new(f: &str) -> CharStream {
+		match File::open(f) {
+			Ok(mut file) => {
+				let mut contents = String::new();
+				file.read_to_string(&mut contents).unwrap();
+				CharStream { contents: contents }
+			}
+			Err(_) => {
+				panic!("Error opening file {}", f);
+			}
+		}
 	}
-	
+
+	pub fn get_contents(&self) -> &String {
+		&self.contents
+	}
+
 	// Returns true if more characters are available, false otherwise.
 	pub fn more_available(&self) -> bool {
+		!self.contents.is_empty()
 	}
 
 	// Returns the next character without consuming it.
-	// Returns None if no more characters are available. 
+	// Returns None if no more characters are available.
 	pub fn peek_next_char(&self) -> Option<char> {
+		self.peek_ahead_char(0)
 	}
 
 	// Returns the kth character ahead in the stream without consuming it.
@@ -26,13 +41,28 @@ impl CharStream {
 	// Returns None if no more characters are available at the position.
 	// The input k cannot be negative.
 	pub fn peek_ahead_char(&self, k: i32) -> Option<char> {
+		match k < 0 {
+			true => None,
+			false => {
+				let ch = self.contents.chars().nth(k as usize).unwrap();
+				Some(ch)
+			}
+		}
 	}
 
 	// Returns the next character and consumes it.
 	// Returns None if no more characters are available.
 	pub fn get_next_char(&mut self) -> Option<char> {
+		match self.peek_next_char() {
+			None => None,
+			Some(ch) => {
+				// consume the first char
+				let mut chars = self.contents.chars();
+				chars.next();
+				chars.next_back();
+				self.contents = String::from(chars.as_str());
+				Some(ch)
+			}
+		}
 	}
 }
-
-
-
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
-//mod character_stream;
-//use character_stream::*;
+mod character_stream;
+use character_stream::*;
 
-mod token;
-use token::*;
-
-
+// mod token;
+// use token::*;
 
 fn main() {
-	let tt = TokenType::OPERATOR;
-	let token = Token::new("+".to_string(), tt, 2, 30);
-	println!("text: {}", token.get_text());
-	println!("token type: {}", token.get_type().as_str());
-	println!("line numer: {}", token.get_line_number());
-	println!("char position: {}", token.get_char_pos());
+	let cc = CharStream::new("format.csv");
+	match cc.peek_next_char() {
+		None => {
+			println!("empty");
+		}
+		Some(ch) => {
+			println!("{}", ch);
+			println!("{}", cc.get_contents());
+		}
+	}
+	// println!("{}", cc.peek_next_char()());
+	// let tt = TokenType::OPERATOR;
+	// let token = Token::new("+".to_string(), tt, 2, 30);
+	// println!("text: {}", token.get_text());
+	// println!("token type: {}", token.get_type().as_str());
+	// println!("line numer: {}", token.get_line_number());
+	// println!("char position: {}", token.get_char_pos());
 }
